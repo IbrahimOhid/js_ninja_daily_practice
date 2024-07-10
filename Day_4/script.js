60e1be7bfa689c6cdfe1d75f7c600bf8b2700130
ibrahimohid <ibrahimohid77@gmail.com>
2024-07-10 22:08:47 +0600

    day_4 practice done

---
+++ Day_4/script.js
@@ -0,0 +1,48 @@
+// Primitive Data
+
+// string
+'Hello'
+"Ibrahim"
+'43'
+"22"
+console.log(typeof ('Hello'));
+
+// Number
+3
+1.2
+-32
+console.log(typeof (2.3));
+
+// Boolean
+true 
+false
+console.log(typeof (true));
+console.log(typeof (false));
+
+// null & Undefined
+null
+undefined
+
+// symbol
+Symbol('ohid')
+Symbol('ohid') !== Symbol('ohid')
+console.log(typeof(Symbol('ohid')));
+
+// Big int
+123n
+console.log(typeof(123n));
+
+// string Practice
+
+console.log('hello world'.length);
+console.log('mohammad'[3]);
+
+console.log(`i love bangladesh. i am a student`);
+
+// working with numbers
+console.log(4 + 2);
+console.log(4 - 2);
+console.log(4 * 2);
+console.log(4 / 2);
+console.log(10 % 3);
+console.log(3 * 3);
