92549c114b9742f0aa7038ed7095ad5bb97b4cb7
ibrahimohid <ibrahimohid77@gmail.com>
2024-07-15 23:10:47 +0600

    day_9 practice done

---
+++ day_8/script.js
@@ -0,0 +1,54 @@
+// Operator
+
+const num1 = 10;
+const num2 = 2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+let num3 = 10;
+num3 += 2;
+console.log(num3);
+
+let num4 = 5;
+num4++;
+++num4;
+console.log(num4);
+
+let num5 = 8;
+num5 -= 2;
+console.log(num5);
+
+let num6 = 7;
+num6--;
+--num6;
+console.log(num6);
+
+let num7 = 2;
+num7 *= 3;
+console.log(num7);
+
+let num8 = 8;
+num8 /= 2;
+console.log(num8);
+
+let num9 = 9;
+num9 %= 2;
+console.log(num9);
+
+let num10 = 10;
+console.log(num10++);
+console.log(num10);
+console.log(++num10);
+console.log(num10);
+
+let num11 = 5;
+console.log(num11--);
+console.log(num11);
+console.log(--num11);
+console.log(num11);
+
---
+++ day_9/script.js
@@ -0,0 +1,28 @@
+console.log(!'same');
+console.log(!!'ibrahim');
+
+console.log(!false);
+console.log(!!false);
+
+console.log(!0);
+console.log(!!0);
+
+console.log(!'');
+console.log(!!'');
+
+console.log(!null);
+console.log(!!null);
+
+console.log(!undefined);
+console.log(!!undefined);
+
+console.log(!NaN);
+console.log(!!NaN);
+
+const isTrue = true;
+console.log(isTrue && 'rifa');
+console.log(isTrue  || 0);
+
+const isFalse = false;
+console.log(isFalse && 'ibrahim');
+console.log(isFalse || 'rifa');
