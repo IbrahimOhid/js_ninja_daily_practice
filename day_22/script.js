dde65c3ff4401163e9a6eaaefc23003f55d48807
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-23 16:08:02 +0600

    day_22 array destructring practice

---
+++ day_22/script.js
@@ -0,0 +1,13 @@
+// Array Destructuring method
+const userInfo = [undefined, 27, 'web designer'];
+
+const [firstName = 'ohid', , profession] = userInfo;
+//console.log(firstName, age, profession);
+console.log(firstName, profession);
+
+
+// object destructuring
+const userProfile = {
+    firstName : 'Mohammad',
+    lastName :
+}
