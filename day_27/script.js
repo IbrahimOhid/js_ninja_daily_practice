d35335881de6d59f87d823bd6eaafc114e26fd57
ibrahimohid <ibrahimohid77@gmail.com>
2024-08-30 23:14:49 +0600

    day 27 practice done

---
+++ day_27/script.js
@@ -0,0 +1,19 @@
+// const para1 = document.querySelector('p');
+// const para1 = document.getElementsByClassName('para1');
+// const para1 = document.getElementById('para1');
+// const para1 = document.querySelector('.para1');
+// console.log(para1);
+
+// accessing attributes
+const ulElem = document.querySelector('ul');
+// console.log(ulElem);
+// console.log('TextContent', ulElem.textContent);
+// console.log('Html', ulElem.innerHTML);
+// console.log('attribute', ulElem.getAttribute('class'));
+// console.log('attribute', ulElem.getAttribute('id'));
+// console.log('className', ulElem.className);
+// console.log('IdName', ulElem.id);
+// console.log('style', ulElem.style);
+
+// const allLink = document.querySelectorAll('a');
+// console.log(allLink);
