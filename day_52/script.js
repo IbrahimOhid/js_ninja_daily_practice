c53aac09cca506c3adb05c66e6f7ac01d0903c48
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-09 22:42:03 +0600

    callback & promise function added

--- day_52/script.js
+++ day_52/script.js
@@ -1,98 +1,142 @@
-// Asynchronous Programming
-
-function one(){
-    setTimeout(()=>{
-        console.log('One');
-        return 1
-    }, 2000)
+// synchronous  💛
+function one() {
+  return 1;
 }
 
-function two(){
-    console.log('Two 1 st');
-    return 2; 
+function two() {
+  return 2;
 }
 
-function three(){
-    setTimeout(()=>{
-        console.log('Three');
-        return 1
-    }, 2000)
+function three() {
+  return 3;
 }
 
-console.log(one());
 console.log(two());
+console.log(one());
 console.log(three());
 
-// callback patten
-
-function four(fn){
-    setTimeout(()=>{
-        console.log('Four');
-        fn(4)
-    }, 2000)
+// asynchronous programming 💛
+function four() {
+  setTimeout(() => {
+    console.log("Four");
+    return 4;
+  }, 2000);
 }
 
-    four((num)=>{
-        console.log(num);
-    })
-
-function five(fn){
-    setTimeout(() => {
-        console.log('Five');
-        fn(5)
-    },2000);
+function five() {
+  setTimeout(() => {
+    console.log("Five");
+    return 5;
+  }, 1000);
 }
 
-    five((num)=>{
-        console.log(num);
-    })
+function six() {
+  setTimeout(() => {
+    console.log("Six");
+    return 6;
+  }, 3000);
+}
 
+console.log(four());
+console.log(five());
+console.log(six());
 
+// asynchronous program method 💛💛💛
 
-function six(fn){
+// callback
+// promise
+// async wait
+
+// callback function 💚
+function seven(fn) {
+  setTimeout(() => {
+    console.log("seven");
+    fn(7);
+  }, 2000);
+}
+
+seven((num) => {
+  console.log(num);
+});
+
+function eight(fn) {
+  setTimeout(() => {
+    console.log("Eight");
+    fn(8);
+  }, 2000);
+}
+
+eight((num) => {
+  console.log(num);
+});
+
+function nine(fn) {
+  setTimeout(() => {
+    console.log("Nine");
+    fn(9);
+  }, 2000);
+}
+
+nine((num) => {
+  console.log(num);
+});
+
+// callback hell 💚💚💚
+function ten(fn) {
+  setTimeout(() => {
+    fn(10);
+  }, 2000);
+}
+
+function eleven(fn) {
+  setTimeout(() => {
+    fn(11);
+  }, 2000);
+}
+
+function twelve(fn) {
+  setTimeout(() => {
+    fn(12);
+  }, 2000);
+}
+
+ten((num1) => {
+  console.log(num1);
+  eleven((num2) => {
+    console.log(num1 + num2);
+    console.log(num2);
+    twelve((num3) => {
+      console.log(num1 + num2 + num3);
+      console.log(num3);
+    });
+  });
+});
+
+// promise function 💚
+function a() {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('Six');
-        fn(6);
+      resolve(5);
     }, 2000);
+  });
 }
 
- six((num)=>{
-        console.log(num);
-    })
+a()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
 
-// callback hell
-
-function seven(sn){
+function b() {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('Seven');
-        sn(7)
+      reject(new Error(505));
     }, 2000);
+  });
 }
 
-function eight(en){
-    setTimeout(() => {
-        console.log('Eight');
-        en(8);
-    }, 2000);
-}
-
-function nine(nn){
-    setTimeout(() => {
-        console.log('Nine');
-        nn(9)
-    }, 2000);
-}
-
-seven((num1)=>{
-    console.log(num1);
-    eight((num2)=>{
-        console.log(num1 + num2);
-        console.log(num2);
-        nine((num3)=>{
-            console.log(num1 + num2 + num3);
-            console.log(num3);
-        })
-    })
-})
-
-
+b()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
