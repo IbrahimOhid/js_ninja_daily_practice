d0d5a9eaea1a13482970c4da2c998180b11cba04
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-13 19:09:32 +0600

    group practice

--- day_55/script.js
+++ day_55/script.js
@@ -1,7 +1,5 @@
 // regExp practice
 const para = 'i am Mohamma ibrahim ohid';
-
-
 // literal Patten
 const regPara = /ohid/ig;
 console.log(regPara.test(para));
@@ -14,3 +12,11 @@ console.log(para.match(regPara));
 console.log(regPara.exec(para));
 // test
 console.log(regPara.test(para));
+// replace
+console.log(para.replace(regPara, 'Ibrahim'));
+
+// Group
+const dates = '2024-10-02, 2022-03-01';
+const regDate = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/g;
+console.log(dates.replace(regDate, '$<day>.$<month>.$<year>'));
+console.log(dates.match(regDate));
