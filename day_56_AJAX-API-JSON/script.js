4962ec621a086b3a420c6e72abba44c9454c32ea
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-13 21:37:39 +0600

    json stringify & json parse added

---
+++ day_56_AJAX-API-JSON/script.js
@@ -0,0 +1,41 @@
+// javascript object 
+// const data = {
+//     name: 'ibrahim',
+//     age: 27,
+//     profession: 'web Engineer',
+//     toBeDesigner: null
+// }
+
+// JSON Object
+// new Date, undefined, function are not support as valid json value
+// const userData = {
+//     'name': 'ibrahim',
+//     'age': 27,
+//     'profession': 'web Engineer',
+//     'toBeDesigner': null;
+// }
+
+// json stringify & json parse
+const data = {
+    name: 'ibrahim',
+    age: 27,
+    profession: 'web Engineer',
+    toBeDesigner: null
+}
+const dataResult = JSON.stringify(data);
+console.log(dataResult);
+console.log(typeof dataResult);
+
+const dataResult2 = JSON.parse(dataResult);
+console.log(dataResult2);
+console.log(typeof dataResult2);
+console.log(dataResult2.name);
+console.log(dataResult2.profession);
+
+
+// fetch
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
+
