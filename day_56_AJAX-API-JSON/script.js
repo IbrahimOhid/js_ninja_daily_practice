73f5ca4f4d50fcaf82fd71277d5ae84cb6dfe6e8
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-15 21:59:29 +0600

    get add post & get single data added

--- day_56_AJAX-API-JSON/script.js
+++ day_56_AJAX-API-JSON/script.js
@@ -16,21 +16,21 @@
 // }
 
 // json stringify & json parse
-const data = {
-  name: "ibrahim",
-  age: 27,
-  profession: "web Engineer",
-  toBeDesigner: null,
-};
-const dataResult = JSON.stringify(data);
-console.log(dataResult);
-console.log(typeof dataResult);
+// const data = {
+//   name: "ibrahim",
+//   age: 27,
+//   profession: "web Engineer",
+//   toBeDesigner: null,
+// };
+// const dataResult = JSON.stringify(data);
+// console.log(dataResult);
+// console.log(typeof dataResult);
 
-const dataResult2 = JSON.parse(dataResult);
-console.log(dataResult2);
-console.log(typeof dataResult2);
-console.log(dataResult2.name);
-console.log(dataResult2.profession);
+// const dataResult2 = JSON.parse(dataResult);
+// console.log(dataResult2);
+// console.log(typeof dataResult2);
+// console.log(dataResult2.name);
+// console.log(dataResult2.profession);
 
 // fetch
 // fetch('https://jsonplaceholder.typicode.com/users')
@@ -52,13 +52,40 @@ console.log(dataResult2.profession);
 // }
 // findUser()
 
-async function receiveUser() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
-receiveUser();
+// async function receiveUser() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// receiveUser();
+
+
+// getting all posts
+// async function getPost() {
+//   try{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     const data = await res.json();
+//     console.log(data.slice(0, 10));
+//   }
+//   catch(err){
+//     console.log('Data not Found');
+//   }
+// }
+// getPost();
+
+// getting single post
+// async function getData() {
+//   try{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts/2')
+//     const data = await res.json();
+//     console.log(data);
+//   }
+//   catch(err){
+//     console.log('Data Not Found');
+//   }
+// }
+// getData();
