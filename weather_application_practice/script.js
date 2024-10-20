7329ce3a479563b822a277423dc36e15b01148de
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-20 07:21:01 +0600

    practice weather app

---
+++ weather_application_practice/script.js
@@ -0,0 +1,24 @@
+const UI={
+  // all selector
+  loadSelector(){
+    const countryInputElm = document.querySelector('#countryInput');
+    const cityInputElm = document.querySelector('#cityInput');
+    const formElm = document.querySelector('form');
+    const wCity = document.querySelector('#w-city');
+    const wImg = document.querySelector('#w-img');
+    const wTemp = document.querySelector('#w-temp');
+    const wPressure = document.querySelector('#w-pressure');
+    const wHumidity = document.querySelector('#w-humidity');
+    return{
+
+    }
+  }
+}
+
+
+
+
+
+const weatherData ={
+
+}
