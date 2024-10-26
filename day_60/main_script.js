092f7e84e19e4401bf8ccac79d0d3c88652edbf4
ibrahimohid <ibrahimohid77@gmail.com>
2024-10-26 22:51:47 +0600

    js module practice

---
+++ day_60/main_script.js
@@ -0,0 +1,8 @@
+import sum, {randomValue} from './sum.js'
+import sub from './substrac.js'
+import mult from './multiply.js'
+
+console.log(sum(20,3));
+console.log(randomValue);
+console.log(sub(30,10));
+console.log(mult(20, 3));
---
+++ day_60/sum.js
@@ -0,0 +1,6 @@
+const randomValue = 30;
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+export {randomValue, sum as default}
