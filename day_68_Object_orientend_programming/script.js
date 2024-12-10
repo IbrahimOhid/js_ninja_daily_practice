6b90ccd680e34950d624413446f9526b2d818c25
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2024-12-10 22:13:02 +0600

    prototype added

--- day_68_Object_orientend_programming/script.js
+++ day_68_Object_orientend_programming/script.js
@@ -16,42 +16,60 @@
 // profile._age = 13;
 // console.log(profile._age);
 
-// constructor function 💚💚💚
-function profile(name, age){
-    this.name = name,
-    this.age = age,
-    this.userInfo = function(){
-        return `${this.name} - ${this.age}`
-    }
+// // constructor function 💚💚💚
+// function profile(name, age){
+//     this.name = name,
+//     this.age = age,
+//     this.userInfo = function(){
+//         return `${this.name} - ${this.age}`
+//     }
+// }
+
+// const ibrahim = new profile('ibrahim', 27);
+// const jisan = new profile('jisan', 25);
+// console.log(ibrahim);
+// console.log(jisan);
+// console.log(ibrahim.userInfo());
+// console.log(jisan.userInfo());
+
+// // string literal
+// const str = 'samim'
+// console.log(str);
+
+// // string literal constructor
+// const strA = new String('samim')
+// console.log(strA);
+// console.log(strA.length);
+// console.log(strA.toLocaleUpperCase());
+
+// // array
+// const arrC = new Array(1,2,3,4)
+// console.log(arrC.length);
+// console.log(arrC);
+
+// // object
+// const nameInfo = new Object({
+//     name1: 'ibrahim',
+//     name2: 'ohid'
+// })
+// console.log(nameInfo);
+// console.log(nameInfo.name1);
+// console.log(nameInfo.name2);
+
+function Person(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
 }
 
-const ibrahim = new profile('ibrahim', 27);
-const jisan = new profile('jisan', 25);
-console.log(ibrahim);
-console.log(jisan);
-console.log(ibrahim.userInfo());
-console.log(jisan.userInfo());
+Person.prototype.fullName = function(){
+    return `${this.firstName} ${this.lastName}`
+}
 
-// string literal
-const str = 'samim'
-console.log(str);
+const ibrahim = new Person('Ibrahim', 'Ohid', 27);
+const rafiya = new Person('Rafiya', 'Kawser', 22);
+const ebna = new Person('Ebna', 'Osman', 20);
 
-// string literal constructor
-const strA = new String('samim')
-console.log(strA);
-console.log(strA.length);
-console.log(strA.toLocaleUpperCase());
-
-// array
-const arrC = new Array(1,2,3,4)
-console.log(arrC.length);
-console.log(arrC);
-
-// object
-const nameInfo = new Object({
-    name1: 'ibrahim',
-    name2: 'ohid'
-})
-console.log(nameInfo);
-console.log(nameInfo.name1);
-console.log(nameInfo.name2);
+console.log(ibrahim.fullName());
+console.log(rafiya.fullName());
+console.log(ebna.fullName());
