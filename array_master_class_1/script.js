455aedad5e6b953bcddd9a995114adc20c0d47b6
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-12 22:55:44 +0600

    array master class 1 practice ongoing

---
+++ array_master_class_1/script.js
@@ -0,0 +1,8 @@
+const fruits = ['orange', 'banana', 'apple'];
+console.log(fruits);
+
+const userName = new Array('Rahim', 'Karim', 'Jisan');
+console.log(userName);
+
+console.log(new Array(2));
+console.log(new Array(3,4,2));
