17dc4f07aa2fff7c15fe076c386a52f06a609eee
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-15 22:36:45 +0600

    array master class 1 done

--- array_master_class_1/script.js
+++ array_master_class_1/script.js
@@ -6,3 +6,34 @@ console.log(userName);
 
 console.log(new Array(2));
 console.log(new Array(3,4,2));
+
+const fruit = ['🍅','🍓','🍌','🍍','🍉','🥕','🌽'];
+const len = fruit.length;
+console.log(len);
+
+console.log(len-1);
+console.log(fruit[len-3]);
+console.log(fruit[len-6]);
+
+for(let i=0; i < fruit.length; i++){
+    console.log(`Element at the Index ${i} is ${fruit[i]}`);
+}
+
+console.log(fruit.push('🥦'));
+console.log(fruit);
+
+console.log(fruit.unshift('🍆'));
+console.log(fruit);
+
+
+console.log(fruit.pop());
+console.log(fruit);
+
+console.log(fruit.shift());
+console.log(fruit);
+
+const newFruit = fruit.slice()
+console.log(newFruit);
+console.log(newFruit === fruit);
+
+console.log(Array.isArray(fruit));
