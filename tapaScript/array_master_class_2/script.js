5fe0d3aeddba18fcb6aaa101be24ade77c85f45a
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-15 22:53:44 +0600

    master class 2 practice ongoing

---
+++ tapaScript/array_master_class_2/script.js
@@ -0,0 +1,9 @@
+// Array Destructuring in JS ⤵
+
+// const [tomato, banana, orange] = ['🍅', '🍌', '🍊'];
+// console.log(tomato);
+// console.log(orange);
+
+const [musrome, banana = '🍌'] = ['🍄']
+console.log(musrome);
+console.log(banana);
