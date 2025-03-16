7aa8e985053184a9e2802cdcfa603105edbf755e
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-16 22:57:50 +0600

    nested array practice

--- tapaScript/array_master_class_2/script.js
+++ tapaScript/array_master_class_2/script.js
@@ -4,6 +4,13 @@
 // console.log(tomato);
 // console.log(orange);
 
-const [musrome, banana = '🍌'] = ['🍄']
-console.log(musrome);
-console.log(banana);
+// const [musrome, banana = '🍌'] = ['🍄']
+// console.log(musrome);
+// console.log(banana);
+
+const [apple, , orange] = ['🍅','🍌','🍊']
+console.log(apple);
+console.log(orange);
+
+const user = ['rahim', 'karim', 'mobin', 'ibrahim', ['jisan', 'riya', 'tiya']];
+console.log(user[4][1]);
