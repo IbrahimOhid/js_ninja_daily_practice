a6f0b7848aa7ca3bbda93cdd9d4d20194a79d767
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-17 22:36:34 +0600

    array_master_class_2 done

--- tapaScript/array_master_class_2/script.js
+++ tapaScript/array_master_class_2/script.js
@@ -12,5 +12,46 @@ const [apple, , orange] = ['🍅','🍌','🍊']
 console.log(apple);
 console.log(orange);
 
-const user = ['rahim', 'karim', 'mobin', 'ibrahim', ['jisan', 'riya', 'tiya']];
-console.log(user[4][1]);
+// const user = ['rahim', 'karim', 'mobin', 'ibrahim', ['jisan', 'firuz', 'tiya']];
+// console.log(user[4][1]);
+
+// const [r, k, m, i, [j, f, t]] = user
+// console.log(r);
+// console.log(m);
+// console.log(j);
+
+
+// ... rest Parameter 💛💛
+const [r, f, ...rest] = ['rahim', 'fahim', 'ibrahim', 'mahir', 'jisan'];
+console.log(r);
+console.log(rest);
+console.log(rest[1]);
+
+
+// Destructure use Cases 💛💛
+let sad = '😭';
+let happy = '😊';
+
+ [happy, sad] = [sad, happy];
+ console.log(happy, sad);
+
+ let isSkilled = true;
+ let notSkilled = false;
+
+[notSkilled, isSkilled]  = [isSkilled, notSkilled];
+console.log(notSkilled, isSkilled);
+
+// ... spread Operator 💛💛
+
+const friendName = ['Hablu', 'Bablu', 'Dablu'];
+const tiktoker = ['alex sabuj', 'price mamun', 'kala mia'];
+
+const result = [...friendName, ...tiktoker];
+console.log(result);
+
+let nums = [1,2,3,4];
+console.log(nums.length);
+
+nums.length = 5;
+console.log(nums.length);
+
