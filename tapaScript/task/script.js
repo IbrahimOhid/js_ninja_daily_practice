3cd5699f3cfc2df149f0c7d77bffdf83fe373f76
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-20 22:43:29 +0600

    task 1 to 2 problem solved

---
+++ tapaScript/task/script.js
@@ -0,0 +1,15 @@
+//  T-001: Create an array of 5 elements using the Array Constructor. 💛💛💛
+
+const myArray = new Array('Rahim', 'Karim', 'ohid', 'ibrahim', 'mohammad');
+console.log(myArray);
+
+//  T-002: Create an array of 3 empty slots. 💛💛💛
+
+const myEmpty = new Array(3);
+console.log(myEmpty);
+
+// T-003: Create an array of 6 elements using the Array literals and access the fourth element in the array using its length property. 💛💛💛
+
+const myFriend = ['jisan', 'mahir', 'shahin', 'touhid', 'faruk', 'saif'];
+const f = myFriend.length - 2;
+console.log(f);
