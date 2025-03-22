9b3a8d6ab2c4a51a86c1d4ca8e95463de0dfa667
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-03-22 23:01:13 +0600

    array_master_class_4 done

---
+++ tapaScript/array_master_class_4/script.js
@@ -0,0 +1,15 @@
+// isArray💛💛💛
+
+const arr = [1, 2, 3];
+console.log(Array.isArray(arr))
+console.log(Array.isArray({}));
+console.log(Array.isArray([]))
+
+
+// Array of 💛💛💛
+
+const userInfo = Array.of('Rahim', true, 'Web Engineer', {name: 'habul', age: 23, isSkilled: true});
+console.log(userInfo)
+
+const a = Array.of(3, 2, true);
+console.log(a)
