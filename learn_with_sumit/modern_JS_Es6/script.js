e09beb183cc51674b3bd8eb4e981dc1ef68fccd8
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-04-13 21:20:30 +0600

    find method practice

--- learn_with_sumit/modern_JS_Es6/script.js
+++ learn_with_sumit/modern_JS_Es6/script.js
@@ -22,3 +22,10 @@ const age = 3;
 
 const result = (age >= 18) ? 'Adult' : 'Child';
 console.log(result);
+
+// Array Find
+
+const numbers = [1, 2, 3, 4, 11, 20, 38];
+
+const results = numbers.find(num => num > 4);
+console.log(results);
