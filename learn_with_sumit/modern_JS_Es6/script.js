4da1afbae0120d4d8ccd866572de31cbf8063107
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-04-13 21:11:23 +0600

    ternary operator practice

--- learn_with_sumit/modern_JS_Es6/script.js
+++ learn_with_sumit/modern_JS_Es6/script.js
@@ -1,1 +1,24 @@
-// ES6 fat Arrow Function
+// ES6 fat Arrow Function 💛💛💛
+
+function number(){
+    return 10;
+}
+console.log(number());
+
+const num = () => 10;
+console.log(num());
+
+// Truthy / Falsy values 💛💛💛
+
+// falsy value ⤵
+
+// "", NaN, undefined, false, 0, null
+
+// ftp://192.168.0.102:2221
+
+// Ternary Operator 💛💛💛
+
+const age = 3;
+
+const result = (age >= 18) ? 'Adult' : 'Child';
+console.log(result);
