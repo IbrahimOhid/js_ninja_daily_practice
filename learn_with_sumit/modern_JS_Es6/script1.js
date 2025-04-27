11df7c56cbf6f69476ad8dd8c71d1b7e2911b79b
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-04-27 23:11:31 +0600

    Think in a JavaScript prototype object create practice

--- learn_with_sumit/modern_JS_Es6/script1.js
+++ learn_with_sumit/modern_JS_Es6/script1.js
@@ -2,12 +2,29 @@
 
 // scope
 
-var x = 20;
+// var x = 20;
 
-function myFun(){
-    // var x = 10;
-    x = 13;
-    console.log(x);
-}
+// function myFun(){
+//     // var x = 10;
+//     x = 13;
+//     console.log(x);
+// }
+// myFun();
+// console.log(x);
+
+// JavaScript Hoisting
+
 myFun();
-console.log(x);
+function myFun(){
+    console.log('I love Javascript');
+}
+
+// object create();
+
+const captain = {
+    name: 'ibrahim',
+    age: 27,
+    position: 'Web Engineer'
+}
+const newPlayer = Object.create(captain);
+console.log(newPlayer.name);
